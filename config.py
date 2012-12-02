1fa1b8b0376ed7babc36a02646ead14aaa552ca2
Daniel Berndt <daniel.berndt@gmail.com>
2012-12-02 13:59:00 +0100

    make config pwd agnostic

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+import os.path
+
+BASEDIR = os.path.dirname(os.path.realpath(__file__))
 FILE = "app.conf"
 
 _conf = {}
@@ -7,7 +10,7 @@ def interprete(value):
   if value.lower()=="false": return False
   return value
 
-f = open(FILE)
+f = open(os.path.join(BASEDIR, FILE))
 for line in map(str.strip,f):
   if not line or line.startswith("#"): continue
   _conf.update(dict((map(str.strip, line.split("=",1)),)))
@@ -18,3 +21,7 @@ f.close()
 
 def get(key):
   return _conf[key]
+
+
+if __name__=="__main__":
+  print _conf
